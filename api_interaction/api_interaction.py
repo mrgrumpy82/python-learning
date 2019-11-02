5919585ad8c690bd84bf74adee85357f1150e536
mr <mrgrumpy@gdockt.net>
2019-11-02 11:33:15 +0000

    basics are working

--- api_interaction/api_interaction.py
+++ api_interaction/api_interaction.py
@@ -1,18 +1,26 @@
 import os
 import sys
 import requests
-import ConfigParser
 import datetime
 
-def get_precip_today_in(config):
-  API_URL = 'http://api.wunderground.com/api/{key}/conditions/q/{state}/{town}.json'
-  r = requests.get(API_URL.format(key=config['api_key'],
-                                  state=config['state'],
-                                  town=config['town']))
-  rainfall = None
-  if r.ok:
-    try:  
-      rainfall = float(r.json()['current_observation']['precip_today_in'])
-    except Exception as ex:
-      rainfall = None
-  return rainfall, r
+from requests.exceptions import HTTPError
+
+def get_rainfall():
+  for URL in ['http://reg.bom.gov.au/fwo/IDV60901/IDV60901.94866.json']:
+    try:
+      response = requests.get(URL)
+      response.raise_for_status()
+    except HTTPError as HTTP_ERROR:
+      print(f'HTTP error occurred: {HTTP_ERROR}')
+    except Exception as ERROR:
+      print(f'Non-HTTP error occured: {ERROR}')
+    else:
+      payload = response.json()
+      query_header = payload["observations"]["header"][0]
+      query_data = payload["observations"]["data"][0]
+      print('Successful GET operation')
+      print(f'{query_header["refresh_message"]}')
+      print(f'Rain since 0900: {query_data["rain_trace"]}')
+      print(f'Current Air Temperature: {query_data["air_temp"]}')
+
+get_rainfall()
